38e6f370fa58795c73c56cab98e5606e4ef65bf3
“Hugh145” <HANTHO300@caledonian.ac.uk>
2023-11-13 22:25:52 +0000

    commit adding the update file Dex2hex

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,30 +1,49 @@
 import java.util.Scanner;
 
-class Dec2Hex
+class Dex2Hex
 {
+    public static String hexConvert(int number) {
+         
+        if (number == 0) {
+            return ("Please enter in value");
+        }
+        if (number < 0) {
+                return ("Please enter in positive numbers only");
+                
+        }
+        
+        char ch[]={‘0’,’1’,’2’,’3’,’4’,’5’,’6’,’7’,’8’,’9’,’A’,’B’,’C’,’D’,’E’,’F’};
+        
+        
+        String hexadecimal="";
+        int rem;
 
-    public static int Arg1;
-
+        while(number != 0)
+        {
+            rem=number%16;
+            hexadecimal= ch[rem] + hexadecimal;
+            number= number/16;
+        }
+        
+        return hexadecimal;
+    }
     public static void main(String args[])
     {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0)
+       if (args.length == 0)
         {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+            System.out.println("Please input a value");
+            return;
         }
-
-        
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-
+        int number;
+        try {
+            number = Integer.parseInt(args[0]);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Invalid format. Please input decimal value.");
+            return;
+        }
+       
+        System.out.println("Converting the Decimal Value " + number + " to Hex…”);
+        System.out.println("Hexadecimal representation is: " + hexConvert(number));
     }
 }
-
