c3bca5b9cc122c0c100c1c742ed510f50240604b
“Hugh145” <HANTHO300@caledonian.ac.uk>
2023-11-13 19:46:30 +0000

    commit coursework Dex file to git

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,34 +1,28 @@
-public class Dex2Hex {
+import java.util.Scanner;
 
-    public static void main(String args[]) {
-        if (args.length == 0) {
-            System.out.println("Error: Please provide an input argument.");
-            return;
+class Dec2Hex
+{
+
+    public static int Arg1;
+
+    public static void main(String args[])
+    {
+        Arg1 = Integer.parseInt(args[0]);
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem, num;
+        num = Arg1;
+        String hexadecimal="";
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+        while(num != 0)
+        {
+            rem=num%16;
+            hexadecimal= ch[rem] + hexadecimal;
+            num= num/16;
         }
 
-        try {
-            int decimalValue = Integer.parseInt(args[0]);
-            if (decimalValue < 0) {
-                System.out.println("Error: Please provide a non-negative integer.");
-                return;
-            }
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
 
-            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-            int rem;
-            int num = decimalValue;
-            String hexadecimal = "";
-
-            System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-            while (num != 0) {
-                rem = num % 16;
-                hexadecimal = ch[rem] + hexadecimal;
-                num = num / 16;
-            }
-
-            System.out.println("Hexadecimal representation is: " + (hexadecimal.isEmpty() ? "0" : hexadecimal));
-        } catch (NumberFormatException e) {
-            System.out.println("Error: Invalid input. Please provide a valid integer.");
-        }
     }
 }
+
