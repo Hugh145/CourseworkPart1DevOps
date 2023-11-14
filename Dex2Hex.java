6302fd11e3c1aaf7760bedc9095ce1797e6e60fb
“Hugh145” <HANTHO300@caledonian.ac.uk>
2023-11-14 22:59:36 +0000

    update Dex2Hex file

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,31 +1,41 @@
 import java.util.Scanner;
-class Dex2Hex {
+
+class DexHex {
+    public static String convertToHexadecimal(int demValue) {
+        if (demValue == 0) {
+            return ("Please input a non-zero value");
+        }
+        if (demValue < 0) {
+            return ("Please input positive numbers only");
+        }
+        char ch[]={'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        String hexadecimal = "";
+        int rem;
+
+        while (demValue != 0) {
+            rem = demValue % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            demValue = demValue / 16;
+        }
+
+        return hexadecimal;
+    }
 
     public static void main(String args[]) {
         if (args.length == 0) {
-            System.out.println("Error: Please enter in a provide an value.");
+            System.out.println("Please input a value");
             return;
         }
 
+        int decimalValue;
         try {
-            int decimalVal = Integer.parseInt(args[0]);
-
-            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-                     
-            int rem, num = decimalVal;
-            String hexadecimal = "";
-
-            System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-            while (num != 0) {
-                rem = num % 16;
-                hexadecimal = ch[rem] + hexadecimal;
-                num = num / 16;
-            }
-
-            System.out.println("Hexadecimal is a : " + (hexadecimal.isEmpty() ? "0" : hexadecimal));
+            decimalValue = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
-            System.out.println("Error: Invalid input. Please provide a valid integer.");
+            System.out.println("Invalid format. Please input a valid decimal value.");
+            return;
         }
+
+        System.out.println("Converting the Decimal Value " + decimalValue + " to Hex...");
+        System.out.println("Hexadecimal representation is: " + convertToHexadecimal(decimalValue));
     }
-}
+} 
