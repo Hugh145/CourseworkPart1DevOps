cf5fbbe78a9790394d14c3e031c0b98674176d20
“Hugh145” <HANTHO300@caledonian.ac.uk>
2023-11-14 21:50:19 +0000

    updating the Dex2Hex

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,49 +1,31 @@
 import java.util.Scanner;
+class Dex2Hex {
 
-class Dex2Hex
-{
-    public static String hexConvert(int number) {
-         
-        if (number == 0) {
-            return ("Please enter in value");
-        }
-        if (number < 0) {
-                return ("Please enter in positive numbers only");
-                
-        }
-        
-        char ch[]={‘0’,’1’,’2’,’3’,’4’,’5’,’6’,’7’,’8’,’9’,’A’,’B’,’C’,’D’,’E’,’F’};
-        
-        
-        String hexadecimal="";
-        int rem;
-
-        while(number != 0)
-        {
-            rem=number%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            number= number/16;
-        }
-        
-        return hexadecimal;
-    }
-    public static void main(String args[])
-    {
-       if (args.length == 0)
-        {
-            System.out.println("Please input a value");
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            System.out.println("Error: Please enter in a provide an value.");
             return;
         }
-        int number;
+
         try {
-            number = Integer.parseInt(args[0]);
+            int decimalVal = Integer.parseInt(args[0]);
+
+            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+                     
+            int rem, num = decimalVal;
+            String hexadecimal = "";
+
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+            while (num != 0) {
+                rem = num % 16;
+                hexadecimal = ch[rem] + hexadecimal;
+                num = num / 16;
+            }
+
+            System.out.println("Hexadecimal is a : " + (hexadecimal.isEmpty() ? "0" : hexadecimal));
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Invalid input. Please provide a valid integer.");
         }
-        catch (NumberFormatException e) {
-            System.out.println("Invalid format. Please input decimal value.");
-            return;
-        }
-       
-        System.out.println("Converting the Decimal Value " + number + " to Hex…”);
-        System.out.println("Hexadecimal representation is: " + hexConvert(number));
     }
 }
